19d4c460bdc02723c7f437a33b3c95479b568f66
Yuta2049 <vinci2049@gmail.com>
2018-12-04 13:51:07 +0400

    0.1

--- src/main/java/ConsumingThread.java
+++ src/main/java/ConsumingThread.java
@@ -7,24 +7,51 @@ public class ConsumingThread extends Thread {
     @Override
     public void run() {
 
-        while (BookingService.quantityOfGeneratedRequests < BookingService.maxQuantityOfGeneratedRequests) {
 
-            //if (BookingService.quantityOfRequestsInQueue > 0) {
+        synchronized (BookingService.requestList) {
 
-            try {
-                Thread.sleep(5000);
+            while (BookingService.quantityOfGeneratedRequests < BookingService.maxQuantityOfGeneratedRequests ||
+                BookingService.requestList.size() > 0) {
+                //currentThread().interrupt();
 
-                if (BookingService.requestList.size() > 0) {
+//                System.out.println("BookingService.quantityOfGeneratedRequests = "+BookingService.quantityOfGeneratedRequests);
+//                System.out.println("BookingService.maxQuantityOfGeneratedRequests = "+BookingService.maxQuantityOfGeneratedRequests);
+//                System.out.println("BookingService.requestList.size() = "+BookingService.requestList.size());
 
-                    BookRequest bookRequest = BookingService.requestList.removeFirst();
-                    System.out.println(currentThread().getName()+". Удалили из очереди элемент: "+bookRequest.getId()+". Размер очереди: "+BookingService.requestList.size());
 
+            //while (BookingService.quantityOfGeneratedRequests < BookingService.maxQuantityOfGeneratedRequests) {
+
+
+
+                //if (BookingService.quantityOfRequestsInQueue > 0) {
+
+                try {
+
+                    if (BookingService.requestList.size() > 0) {
+
+                        BookRequest bookRequest = BookingService.requestList.removeFirst();
+                        BookingService.requestList.notifyAll();
+
+                        System.out.println(currentThread().getName() + ". Удалили из очереди элемент: " + bookRequest.getId() + ". Размер очереди: " + BookingService.requestList.size());
+
+                    }
+
+                    Thread.sleep(5000);
+
+
+                    // Немножко бред?
+                    if (BookingService.quantityOfGeneratedRequests < BookingService.maxQuantityOfGeneratedRequests ||
+                            BookingService.requestList.size() > 0) {
+                        BookingService.requestList.wait();
+                    }
+
+
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Ошибка при отправке потока в sleep, Поток: " + currentThread().getName());
                 }
-
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-                System.out.println("Ошибка при отправке потока в sleep, Поток: " + currentThread().getName());
             }
+            System.out.println(currentThread().getName()+" Консьюмер - вышли из цикла");
         }
     }
 }
--- src/main/java/ProducingThread.java
+++ src/main/java/ProducingThread.java
@@ -3,16 +3,27 @@ public class ProducingThread extends Thread {
     @Override
     public void run() {
 
-        while (BookingService.quantityOfGeneratedRequests < BookingService.maxQuantityOfGeneratedRequests) {
 
-            if (BookingService.requestList.size() < BookingService.maxQuantityOfRequestsInQueue) {
-                BookRequest bookRequest = new BookRequest();
-                BookingService.requestList.add(bookRequest);
-                BookingService.quantityOfGeneratedRequests++;
-                System.out.println(currentThread().getName()+". Сгенерировали добавили в очередь элемент: "+bookRequest.getId()+". Размер очереди: "+BookingService.requestList.size());
+        synchronized (BookingService.requestList) {
 
+            while (BookingService.quantityOfGeneratedRequests < BookingService.maxQuantityOfGeneratedRequests) {
 
+                if (BookingService.requestList.size() < BookingService.maxQuantityOfRequestsInQueue) {
+                    BookRequest bookRequest = new BookRequest();
+                    BookingService.requestList.add(bookRequest);
+                    BookingService.quantityOfGeneratedRequests++;
+                    System.out.println(currentThread().getName() + ". Сгенерировали добавили в очередь элемент: " + bookRequest.getId() + ". Размер очереди: " + BookingService.requestList.size());
+                    BookingService.requestList.notifyAll();
+                } else {
+                    try {
+                        BookingService.requestList.wait();
+                    } catch (Exception e) {
+                        System.out.println("Ошибка при wait в продьюсере");
+                    }
+                }
             }
+
+            System.out.println(currentThread().getName()+" Продьюсер - вышли из цикла");
         }
     }
 }
